66ffdb717c4a3b9e834c4300c67d46af70d2dde7
TrangCH <camtrang@web.de>
2021-11-17 23:51:22 +0100

    Added images and started to work on slide show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import { setInterval } from 'timers'; gelöscht.
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'camtrang';
+export class AppComponent implements OnInit {
+  images = ['Mühlbach2.jpg', 'Mühlbach9.jpg', 'Mühlbach16.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  /**
+   * OnInit-Funktion wird immer am Anfang, wenn unsere app components lädt, ausgeführt.
+   */
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+
 }
