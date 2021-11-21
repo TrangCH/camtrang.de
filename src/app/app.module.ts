29ee634a039fe7ada239a7de84a4ac4d68b3b39c
TrangCH <camtrang@web.de>
2021-11-22 00:19:08 +0100

    Main area

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SlideshowComponent } from './slideshow/slideshow.component';
 import { MenuComponent } from './menu/menu.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { MainAreaComponent } from './main-area/main-area.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlideshowComponent,
-    MenuComponent
+    MenuComponent,
+    ImprintComponent,
+    MainAreaComponent
   ],
   imports: [
     BrowserModule,
--- src/app/imprint/imprint.component.spec.ts
+++ src/app/imprint/imprint.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SlideshowComponent } from './slideshow.component';
+import { ImprintComponent } from './imprint.component';
 
-describe('SlideshowComponent', () => {
-  let component: SlideshowComponent;
-  let fixture: ComponentFixture<SlideshowComponent>;
+describe('ImprintComponent', () => {
+  let component: ImprintComponent;
+  let fixture: ComponentFixture<ImprintComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SlideshowComponent ]
+      declarations: [ ImprintComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SlideshowComponent);
+    fixture = TestBed.createComponent(ImprintComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
