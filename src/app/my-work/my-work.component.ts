8e399cce448405fabadc71d0bf381daed18c560f
TrangCH <camtrang@web.de>
2022-01-25 21:47:00 +0100

    my-work.component: visitProject(url: string)

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -36,48 +36,51 @@ export class MyWorkComponent implements OnInit {
     this.filtered = this.allProjects.filter(project => project.category === category);
   }
 
+  visitProject(url: string) {
+    window.open(url, "_blank");
+  }
 
-  allProjects = [ // Hier stand vorher allProjects
+  allProjects = [ 
     {
       img: 'assets/img/Mühlbach2.jpg',
       title: 'Quiz-App',
       shortInformation: 'JavaScript - Quiz.',
-      link: 'https://www.cam-huy-trang.developerakademie.com/assets/portfolio/quiz_app/index.html',
+      url: './assets/portfolio/quiz_app/index.html',
       category: 'JavaScript',
     },
     {
       img: 'assets/img/Mühlbach9.jpg',
       title: 'Join-Kanban-Board',
-      shortInformation: 'JavaScript - based project management',
-      link: 'http://gruppe-95.developerakademie.com/',
+      shortInformation: 'JavaScript and group project - based project management',
+      url: 'http://gruppe-95.developerakademie.com/',
       category: 'JavaScript',
     },
     {
       img: 'assets/img/Mühlbach10.jpg',
       title: 'EL POLLO LOCO',
       shortInformation: 'JavaScript - based jump and run game.',
-      link: 'https://www.cam-huy-trang.developerakademie.com/assets/portfolio/el_pollo_loco/index.html',
+      url: 'https://www.cam-huy-trang.developerakademie.com/assets/portfolio/el_pollo_loco/index.html',
       category: 'JavaScript',
     },
     {
       img: 'assets/img/Mühlbach16.jpg',
       title: 'Ring of fire',
       shortInformation: 'A drinking card game',
-      link: 'https://www.cam-huy-trang.developerakademie.com/assets/portfolio/ringoffire/index.html',
+      url: 'https://www.cam-huy-trang.developerakademie.com/assets/portfolio/ringoffire/index.html',
       category: 'Angular',
     },
     {
       img: 'assets/img/Mühlbach16.jpg',
       title: 'Simple-CRM',
       shortInformation: 'It is a simple Customer-Relationship-Management-System',
-      link: 'https://www.cam-huy-trang.developerakademie.com/assets/portfolio/simple-crm/index.html',
+      url: 'https://www.cam-huy-trang.developerakademie.com/assets/portfolio/simple-crm/index.html',
       category: 'Angular',
     },
     {
       img: 'assets/img/Mühlbach16.jpg',
       title: 'Pokédex',
       shortInformation: "A tabular overview (type, strengths and abilities) of the Pokémon`s",
-      link: 'https://www.cam-huy-trang.developerakademie.com/assets/portfolio/pokédex/index.html',
+      url: 'https://www.cam-huy-trang.developerakademie.com/assets/portfolio/pokédex/index.html',
       category: 'API',
     },
   ];
