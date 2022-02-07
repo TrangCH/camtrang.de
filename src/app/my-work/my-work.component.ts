9d22c63ed76283f777e5983bb5d013d083bd1121
TrangCH <camtrang@web.de>
2022-02-07 18:55:18 +0100

    my-work.component: mouseover and mouseout, in progress

--- src/app/my-work/my-work.component.ts
+++ src/app/my-work/my-work.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyWorkComponent implements OnInit {
   id: any;
+  fadeInAnimation = false;
 
   constructor() { }
   ngOnInit(): void {
